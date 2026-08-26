bba6ab22a98672bdee3baa803545aef49d4730c1
Your Name <you@example.com>
2026-08-26 10:47:23 -0400

    Completing the list all accounts task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,3 +139,23 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_list_all_accounts(self):
+        """It should List all Account"""
+        account = self._create_accounts(5)
+        resp = self.client.get(
+            f"{BASE_URL}", content_type="application/json"
+        ) 
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_list_all_accounts_empty(self):
+        """It should return an empty accounts list"""
+        account = []
+        resp = self.client.get(
+            f"{BASE_URL}", content_type="application/json"
+        ) 
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 0)
