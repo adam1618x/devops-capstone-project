7ba536819a14e2b5a6686ba5ff9776e9ac1286b1
Your Name <you@example.com>
2026-08-26 13:49:09 -0400

    Fixing ci build

--- service/routes.py
+++ service/routes.py
@@ -61,11 +61,12 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_all_accounts():
     """
     List all Accounts
-    This endpoint will list all Accounts 
+    This endpoint will list all Accounts
     """
     app.logger.info("Request to list all Accounts")
 
@@ -82,6 +83,7 @@ def list_all_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -96,10 +98,10 @@ def get_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -120,6 +122,7 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -131,9 +134,8 @@ def delete_accounts(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-    
+
     return "", status.HTTP_204_NO_CONTENT
-    
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
